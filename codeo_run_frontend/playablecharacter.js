c217755c160b1424e7af14c17162f1485beba736
Mark R Pothecary <markpothers@hotmail.com>
2019-04-15 15:47:57 -0500

    1

---
+++ codeo_run_frontend/playablecharacter.js
@@ -0,0 +1,15 @@
+class PlayableCharacter {
+
+    constructor(name, x=100, y=100, points=0, health=100, life_time_points=0){
+        this.name = name
+        this.x = x
+        this.y = y
+        this.points = points
+        this.health = health
+        this.life_time_points = life_time_points
+    }
+
+
+    
+
+}
