a9d44dcfd35da5fe11a01e0023599c8bfcec73b0
Mark R Pothecary <markpothers@hotmail.com>
2019-04-17 14:32:10 -0500

    objects now have hieght

--- codeo_run_backend/db/migrate/20190415184601_create_playable_characters.rb
+++ codeo_run_backend/db/migrate/20190415184601_create_playable_characters.rb
@@ -2,9 +2,11 @@ class CreatePlayableCharacters < ActiveRecord::Migration[5.2]
   def change
     create_table :playable_characters do |t|
       t.string :name
+      t.integer :x
+      t.
       t.integer :health
       t.integer :points
-      t.integer :life_time_points
+      t.integer :special
 
       t.timestamps
     end
