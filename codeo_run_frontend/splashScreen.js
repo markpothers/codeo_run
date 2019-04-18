04962ed781bcf310ccc4c838c88b0bfa90364143
zepher2211 <zepher2211@gmail.com>
2019-04-18 09:28:50 -0500

    splash screen

---
+++ codeo_run_frontend/splashScreen.js
@@ -0,0 +1,34 @@
+const splashScreen = () => {
+  const splash = document.querySelector('#splash')
+  const splashDraw = splash.getContext('2d')
+  const logo = document.createElement('img')
+  const startButton = document.createElement('img')
+  logo.src = 'assets/Logo/logo_transparent.png'
+  startButton.src = 'assets/ui/BlueButton.png'
+  splash.append(logo)
+
+  splash.width = window.innerWidth;
+  splash.height = 400;
+
+  splashDraw.fillStyle = 'orange';
+
+  splashDraw.fillRect(0, 0, splash.width, splash.height)
+
+  logo.addEventListener('load', () => {
+    let button = document.createElement('canvas')
+    let buttonContext = button.getContext('2d')
+    button.style.zIndex = 10
+    button.width = startButton.width
+    button.height = startButton.height
+    button.style.position = 'absolute'
+    button.style.left = `${(splash.width-startButton.width)/2}px`
+    button.style.top = `388px`
+    document.body.append(button)
+    logo.width = logo.width/3
+  splashDraw.drawImage(logo, (splash.width-logo.width)/2, 0, 400, 400);
+  splashDraw.drawImage(startButton, (splash.width-startButton.width)/2, 300, startButton.width, startButton.height);
+  button.addEventListener('click', () => {
+    splash.style.display = 'none'
+  })
+})
+}
